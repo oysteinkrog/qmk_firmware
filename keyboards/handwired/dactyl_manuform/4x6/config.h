7054118990e9d3d7d72915ab875dc798b751d61f
brickbots <rich@brickbots.com>
2020-01-29 08:10:39 -0800

    add/enable use of oled display

--- keyboards/handwired/dactyl_manuform/4x6/config.h
+++ keyboards/handwired/dactyl_manuform/4x6/config.h
@@ -42,3 +42,6 @@ along with this program.  If not, see <http://www.gnu.org/licenses/>.
 // WS2812 RGB LED strip input and number of LEDs
 #define RGB_DI_PIN D3
 #define RGBLED_NUM 12
+
+#define OLED_DISPLAY_ADDRESS 0x78
+#define OLED_DISPLAY_128X64
