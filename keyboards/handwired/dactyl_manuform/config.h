8c6054feeff37c36c5827d9f313c2709c6c60760
Øystein Krog <oystein.krog@gmail.com>
2020-03-04 14:23:17 +0100

    reduce tapping term to 150

--- keyboards/handwired/dactyl_manuform/config.h
+++ keyboards/handwired/dactyl_manuform/config.h
@@ -67,6 +67,6 @@ along with this program.  If not, see <http://www.gnu.org/licenses/>.
 //#define NO_ACTION_MACRO
 //#define NO_ACTION_FUNCTION
 //
-//#define TAPPING_TERM 200
+#define TAPPING_TERM 150
 
 //#define COMBO_COUNT 1
