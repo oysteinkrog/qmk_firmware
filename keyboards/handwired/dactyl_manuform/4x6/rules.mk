1d163fd7d3fed3eb1ae6854403857c2bde566d17
Øystein Krog <oystein.krog@gmail.com>
2020-03-04 14:22:47 +0100

    disable oled stuff

--- keyboards/handwired/dactyl_manuform/4x6/rules.mk
+++ keyboards/handwired/dactyl_manuform/4x6/rules.mk
@@ -15,8 +15,8 @@ AUDIO_ENABLE = no           # Audio output on port C6
 UNICODE_ENABLE = no         # Unicode
 BLUETOOTH_ENABLE = no       # Enable Bluetooth with the Adafruit EZ-Key HID
 RGBLIGHT_ENABLE = no       # Enable WS2812 RGB underlight. 
-OLED_DRIVER_ENABLE = yes
-WPM_ENABLE = yes
+OLED_DRIVER_ENABLE = no
+WPM_ENABLE = no
 
 # Do not enable SLEEP_LED_ENABLE. it uses the same timer as BACKLIGHT_ENABLE
 SLEEP_LED_ENABLE = no    # Breathing sleep LED during USB suspend
