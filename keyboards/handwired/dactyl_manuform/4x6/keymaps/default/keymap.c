71876c5234a49197b517f487e03e51f52078d7c1
Øystein Krog <oystein.krog@gmail.com>
2020-08-05 13:16:15 +0200

    keymap: fix numpad

--- keyboards/handwired/dactyl_manuform/4x6/keymaps/default/keymap.c
+++ keyboards/handwired/dactyl_manuform/4x6/keymaps/default/keymap.c
@@ -114,9 +114,9 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
  *                                         `-------+-------'     `-------+-------'
  */
 [_L2] = LAYOUT(
-    _______,_______,  KC_F7,  KC_F8,  KC_F9, KC_F10,                       KC_EQL,   KC_7,   KC_8,   KC_9,KC_PLUS,_______,
-    _______,_______,  KC_F4,  KC_F5,  KC_F6, KC_F11,                         KC_0,   KC_4,   KC_5,   KC_6,KC_MINS,_______,
-    _______,_______,  KC_F1,  KC_F2,  KC_F3, KC_F12,                       KC_DOT,   KC_1,   KC_2,   KC_3,KC_COMM,_______,
+    _______,_______,  KC_F7,  KC_F8,  KC_F9, KC_F10,                       KC_EQL,   KC_7,   KC_8,   KC_9,KC_COMM,_______,
+    _______,_______,  KC_F4,  KC_F5,  KC_F6, KC_F11,                      KC_PLUS,   KC_4,   KC_5,   KC_6,KC_DOT ,_______,
+    _______,_______,  KC_F1,  KC_F2,  KC_F3, KC_F12,                      KC_MINS,   KC_1,   KC_2,   KC_3,KC_0   ,_______,
                     _______,_______,_______,_______,_______,      _______,_______,_______,_______,_______,
                                               RESET,_______,      _______,  RESET
 ),
